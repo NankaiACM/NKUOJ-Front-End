d22b2f0c7b10d20e2a805d6a1dbf7d96b2ef201d
listerily <listerily@outlook.com>
2022-02-10 17:33:23 +0800

    fix: lose login information on closing webpage

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  plugins: [createPersistedState({
-    storage: window.sessionStorage
-  })],
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => Cookies.get(key),
+        setItem: (key, value) =>
+          Cookies.set(key, value, { expires: 3, secure: true }),
+        removeItem: (key) => Cookies.remove(key),
+      },
+    })
+  ],
   state: {
     userData: {
       isUserLogin: false,
