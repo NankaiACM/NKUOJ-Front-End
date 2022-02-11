0a9b67080dc5a39575ce7374bbc59f26b0c20c41
listerily <listerily@outlook.com>
2022-02-11 12:49:47 +0800

    api fix: problems in assignment page

--- src/pages/assignment.vue
+++ src/pages/assignment.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="container">
-    <b-overlay :show="isLoading" rounded="sm">
+    <b-overlay :show="isDetailsLoading" rounded="sm">
       <div class="jumbotron text-center">
         <h1 class="display-4">{{assignmentData.name}}</h1>
         <p class="lead">{{assignmentData.courseName}}</p>
         <hr class="my-4">
-        <p>{{assignmentData.during}} 截止</p>
+        <p>{{assignmentData.begin}} 开始 {{assignmentData.end}} 截止</p>
+        <p v-if="assignmentData.open" class="text-success">正在进行，可提交</p>
+        <p v-else class="text-success">未开始或已截至，无法提交</p>
       </div>
+    </b-overlay>
+    <b-overlay :show="isProblemsLoading" rounded="sm">
       <div class="card">
         <div class="card-body">
           <div class="container">
@@ -16,15 +20,14 @@
           <div class="container">
             <h5 class="card-title mt-4">完成题目</h5>
             <b-list-group>
-              <b-list-group-item :href="'/assignment/' + assignmentId + '/problem/' + problem.id" class="flex-column align-items-start" v-for="problem in assignmentData.problems" v-bind:key="problem.id">
+              <b-list-group-item :href="'/problem/' + problem.pid" class="flex-column align-items-start" v-for="problem in problemsData" v-bind:key="problem.pid">
                 <div class="d-flex w-100 justify-content-between">
                   <h5 class="mb-1">{{problem.name}}</h5>
-                  <small class="text-muted">分数：{{problem.score}}</small>
                 </div>
 
                 <small class="text-muted" v-if="problem.status === 0">未提交</small>
-                <small class="text-warning" v-if="problem.status === 1">未通过</small>
-                <small class="text-success" v-if="problem.status === 2">已通过</small>
+                <small class="text-warning" v-else-if="problem.status === 1">未通过</small>
+                <small class="text-success" v-else>已通过</small>
               </b-list-group-item>
             </b-list-group>
           </div>
@@ -41,16 +44,20 @@ export default {
     return {
       assignmentId: '',
       assignmentData: {},
-      isLoading: true,
-      answerText: ''
+      problemsData: [],
+      isDetailsLoading: true,
+      isProblemsLoading: true
     }
   },
   methods: {
     loadAssignmentData: function () {
       this.$http.get(`${window.backendOrigin}/api/assignment/id/${this.assignmentId}`).then(res => {
         this.assignmentData = res.data
-        this.assignmentData.during = this.assignmentData.during.substr(24, 19)
-        this.isLoading = false
+        this.isDetailsLoading = false
+      })
+      this.$http.get(`${window.backendOrigin}/api/problem/assignment/${this.assignmentId}`).then(res => {
+        this.problemsData = res.data
+        this.isProblemsLoading = false
       })
     }
   },
