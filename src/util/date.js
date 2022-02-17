b03446ee4f40eaa9839818371ef9173c0b57b3d5
listerily <listerily@outlook.com>
2022-02-17 12:40:48 +0800

    fix: time zone support is based on the client settings now

--- src/util/date.js
+++ src/util/date.js
@@ -1,4 +1,4 @@
 export default function (dateString) {
   // convert from UTC+0 to UTC+8
-  return new Date(Date.parse(dateString) + 3600000).toLocaleString()
+  return new Date(Date.parse(dateString) - 60000 * new Date().getTimezoneOffset()).toLocaleString()
 }
