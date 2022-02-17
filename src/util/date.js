2f31882b8574e8937cb5760452581495ad88c170
listerily <listerily@outlook.com>
2022-02-17 12:46:57 +0800

    fix: removed extra timezone offset

--- src/util/date.js
+++ src/util/date.js
@@ -1,4 +1,4 @@
 export default function (dateString) {
   // convert from UTC+0 to UTC+8
-  return new Date(Date.parse(dateString) - 60000 * new Date().getTimezoneOffset()).toLocaleString()
+  return new Date(Date.parse(dateString)).toLocaleString()
 }
