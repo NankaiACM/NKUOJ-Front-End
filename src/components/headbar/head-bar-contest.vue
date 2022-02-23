0dceff5ac4242fb38391bbba1c2caab5ab457747
listerily <listerily@outlook.com>
2022-02-23 14:49:05 +0800

    fix: removed redundant div

--- src/components/headbar/head-bar-contest.vue
+++ src/components/headbar/head-bar-contest.vue
@@ -1,34 +1,31 @@
 <template>
-  <div>
-    <div class="container-fluid bg-light shadow-sm">
-      <div class="container">
-        <b-navbar class="navbar-expand-lg" toggleable="lg" type="light" variant="light">
-          <a href="/">
-            <img class="slogan" src="../../assets/nkuoj-slogan.svg" alt="Nankai University Online Judge">
-          </a>
+  <div class="container-fluid bg-light shadow-sm">
+    <div class="container">
+      <b-navbar class="navbar-expand-lg" toggleable="lg" type="light" variant="light">
+        <a href="/">
+          <img class="slogan" src="../../assets/nkuoj-slogan.svg" alt="Nankai University Online Judge">
+        </a>
 
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item>算法导论考试</b-nav-item>
-            </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item>算法导论考试</b-nav-item>
+          </b-navbar-nav>
 
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item-dropdown right>
-                <template #button-content>
-                  <em>{{$store.getters.getUserData.nickname}}</em>
-                </template>
-                <b-dropdown-item href="/admin/home" v-if="$store.getters.isAdministrator">管理面板</b-dropdown-item>
-                <b-dropdown-item href="/logout">退出</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-      </div>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                <em>{{$store.getters.getUserData.nickname}}</em>
+              </template>
+              <b-dropdown-item href="/admin/home" v-if="$store.getters.isAdministrator">管理面板</b-dropdown-item>
+              <b-dropdown-item href="/logout">退出</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
   </div>
-
 </template>
 
 <script>
--- src/components/headbar/head-bar-universal.vue
+++ src/components/headbar/head-bar-universal.vue
@@ -1,35 +1,33 @@
 <template>
-  <div>
-    <div class="container-fluid bg-light shadow-sm">
-      <div class="container">
-        <b-navbar class="navbar-expand-lg" toggleable="lg" type="light" variant="light">
-          <a href="/">
-            <img class="slogan" src="../../assets/nkuoj-slogan.svg" alt="Nankai University Online Judge">
-          </a>
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item @click="$emit('toHome')">主页</b-nav-item>
-              <b-nav-item @click="$emit('toProblem')">题目</b-nav-item>
-              <b-nav-item @click="$emit('toStatus')">记录</b-nav-item>
-              <b-nav-item @click="$emit('toCourse')">课程</b-nav-item>
-              <!--<b-nav-item @click="$emit('toContest')">竞考</b-nav-item>-->
-              <!--<b-nav-item @click="$emit('toDiscuss')">讨论</b-nav-item>-->
-              <!--<b-nav-item @click="$emit('toVideos')">视频</b-nav-item>-->
-            </b-navbar-nav>
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item-dropdown right>
-                <template #button-content>
-                  <em>{{$store.getters.getUserData.nickname}}</em>
-                </template>
-                <b-dropdown-item href="/profile">用户中心</b-dropdown-item>
-                <b-dropdown-item href="/admin/home" v-if="$store.getters.isAdministrator">管理面板</b-dropdown-item>
-                <b-dropdown-item href="/logout">退出</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-      </div>
+  <div class="container-fluid bg-light shadow-sm">
+    <div class="container">
+      <b-navbar class="navbar-expand-lg" toggleable="lg" type="light" variant="light">
+        <a href="/">
+          <img class="slogan" src="../../assets/nkuoj-slogan.svg" alt="Nankai University Online Judge">
+        </a>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item @click="$emit('toHome')">主页</b-nav-item>
+            <b-nav-item @click="$emit('toProblem')">题目</b-nav-item>
+            <b-nav-item @click="$emit('toStatus')">记录</b-nav-item>
+            <b-nav-item @click="$emit('toCourse')">课程</b-nav-item>
+            <!--<b-nav-item @click="$emit('toContest')">竞考</b-nav-item>-->
+            <!--<b-nav-item @click="$emit('toDiscuss')">讨论</b-nav-item>-->
+            <!--<b-nav-item @click="$emit('toVideos')">视频</b-nav-item>-->
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template #button-content>
+                <em>{{$store.getters.getUserData.nickname}}</em>
+              </template>
+              <b-dropdown-item href="/profile">用户中心</b-dropdown-item>
+              <b-dropdown-item href="/admin/home" v-if="$store.getters.isAdministrator">管理面板</b-dropdown-item>
+              <b-dropdown-item href="/logout">退出</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
   </div>
 </template>
