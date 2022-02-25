786e2804d023984273d05e29d5eb9e14d1b2ac65
listerily <listerily@outlook.com>
2022-02-25 15:08:21 +0800

    feat: added a minimum constraint to number input

--- src/pages/status.vue
+++ src/pages/status.vue
@@ -6,10 +6,10 @@
     <b-card bg-variant="light">
       <b-form-group label-cols-lg="3" label="筛选" label-class="font-weight-bold pt-0" class="mb-0">
         <b-form-group label="用户 UID:" label-for="input-uid" label-cols-sm="3" label-align-sm="right" label-size="sm">
-          <b-form-input id="input-uid" type="number" size="sm" placeholder="所有用户" v-model="filters.uid" @change="updateFilters"></b-form-input>
+          <b-form-input id="input-uid" type="number" size="sm" placeholder="所有用户" min="1" v-model="filters.uid" @change="updateFilters"></b-form-input>
         </b-form-group>
         <b-form-group label="题目 PID:" label-for="input-pid" label-cols-sm="3" label-align-sm="right" label-size="sm">
-          <b-form-input id="input-pid" type="number" size="sm" placeholder="所有题目" v-model="filters.pid" @change="updateFilters"></b-form-input>
+          <b-form-input id="input-pid" type="number" size="sm" placeholder="所有题目" min="1" v-model="filters.pid" @change="updateFilters"></b-form-input>
         </b-form-group>
       </b-form-group>
     </b-card>
