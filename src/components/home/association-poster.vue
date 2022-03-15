b14e0997a88991639dcb2b565932ce324cc50f10
listerily <listerily@outlook.com>
2022-03-15 15:02:50 +0800

    association poster would auto-fold the qr code

--- src/components/home/association-poster.vue
+++ src/components/home/association-poster.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <b-card title="南开大学ACM算法协会" sub-title="纳新喵~">
-      <img class="img-fluid" src="../../assets/qq-group.jpg" alt="QQ Group ID: 1001918732">
+      <b-link v-b-toggle.collapse-1 class="text-decoration-none">显示二维码</b-link>
+      <b-collapse id="collapse-1" class="mt-2">
+        <img class="img-fluid" src="../../assets/qq-group.jpg" alt="QQ Group ID: 1001918732">
+      </b-collapse>
     </b-card>
   </div>
 </template>
