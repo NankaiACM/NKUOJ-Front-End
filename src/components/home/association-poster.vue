fdedafbba1497a2331d6ee6af8f8ebfde49d7f89
listerily <listerily@outlook.com>
2022-03-16 11:56:20 +0800

    feat: updated link style in the association ad

--- src/components/home/association-poster.vue
+++ src/components/home/association-poster.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-card title="南开大学ACM算法协会" sub-title="纳新喵~">
-      <b-link v-b-toggle.collapse-1 class="text-decoration-none">显示二维码</b-link>
+      <b-link v-b-toggle.collapse-1 class="text-decoration-none text-muted">显示二维码</b-link>
       <b-collapse id="collapse-1" class="mt-2">
         <img class="img-fluid" src="../../assets/qq-group.jpg" alt="QQ Group ID: 1001918732">
       </b-collapse>
