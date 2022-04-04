2244362c87f6f230893bdaea330246ea3529ec99
listerily <listerily@outlook.com>
2022-04-04 19:47:12 +0800

    fix: better button style on problem header

--- src/components/problem/problem-header.vue
+++ src/components/problem/problem-header.vue
@@ -11,37 +11,35 @@
       <h6 class="text-center">题目信息 #{{pid}}</h6>
       <div class="row d-flex justify-content-center">
         <div class="btn-group m-2" role="group">
-          <button type="button" class="btn btn-sm btn-info disabled">测试点数量</button>
-          <button type="button" class="btn btn-sm btn-light disabled">{{ problemInfo.cases }}</button>
+          <button type="button" disabled class="btn btn-sm btn-info">测试点数量</button>
+          <button type="button" disabled class="btn btn-sm btn-light">{{ problemInfo.cases }}</button>
         </div>
         <div class="btn-group m-2" role="group">
-          <button type="button" class="btn btn-sm btn-primary disabled">时间限制</button>
-          <button type="button" class="btn btn-sm btn-light disabled">{{ problemInfo.timeLimit }} ms</button>
+          <button type="button" disabled class="btn btn-sm btn-primary">时间限制</button>
+          <button type="button" disabled class="btn btn-sm btn-light">{{ problemInfo.timeLimit }} ms</button>
         </div>
         <div class="btn-group m-2" role="group">
-          <button type="button" class="btn btn-sm btn-warning disabled">空间限制</button>
-          <button type="button" class="btn btn-sm btn-light disabled">{{ problemInfo.memoryLimit }} KiB</button>
+          <button type="button" disabled class="btn btn-sm btn-warning">空间限制</button>
+          <button type="button" disabled class="btn btn-sm btn-light">{{ problemInfo.memoryLimit }} KiB</button>
         </div>
         <div class="btn-group m-2" role="group">
-          <button type="button" class="btn btn-sm btn-success disabled">完成状态</button>
-          <button type="button" class="btn btn-sm btn-light disabled">{{['未提交', '未通过', '已通过'][problemInfo.status]}}
+          <button type="button" disabled class="btn btn-sm btn-success">完成状态</button>
+          <button type="button" disabled class="btn btn-sm btn-light">{{['未提交', '未通过', '已通过'][problemInfo.status]}}
           </button>
         </div>
         <div class="btn-group m-2" role="group">
-          <button type="button" class="btn btn-sm btn-primary disabled">通过率</button>
-          <button type="button" class="btn btn-sm btn-light disabled">{{ problemInfo.submitAc }} /
+          <button type="button" disabled class="btn btn-sm btn-primary">通过率</button>
+          <button type="button" disabled class="btn btn-sm btn-light">{{ problemInfo.submitAc }} /
             {{ problemInfo.submitAll }}
           </button>
         </div>
       </div>
       <div class="row d-flex justify-content-center">
-        <div class="btn-group m-2" role="group" v-if="problemInfo.specialJudge">
-          <button type="button" class="btn btn-sm btn-outline-warning"
-                  v-b-popover.hover.bottom="'本题启用 Special Judge。所有输出会使用特别程序校验，而不是与标准输出进行比对。'">Special Judge</button>
+        <div class="btn-group m-2" role="group" v-if="problemInfo.specialJudge" v-b-popover.hover.bottom="'本题启用 Special Judge。所有输出会使用特别程序校验，而不是与标准输出进行比对。'">
+          <button type="button" disabled class="btn btn-sm btn-outline-warning">Special Judge</button>
         </div>
-        <div class="btn-group m-2" role="group" v-if="problemInfo.detailJudge">
-          <button type="button" class="btn btn-sm btn-outline-info"
-                  v-b-popover.hover.bottom="'本题启用 Detail Judge。所有测试点都会被评测，而不是遇到第一个非正确运行即停止评测。'">Detail Judge</button>
+        <div class="btn-group m-2" role="group" v-if="problemInfo.detailJudge" v-b-popover.hover.bottom="'本题启用 Detail Judge。所有测试点都会被评测，而不是遇到第一个非正确运行即停止评测。'">
+          <button type="button" disabled class="btn btn-sm btn-outline-info">Detail Judge</button>
         </div>
       </div>
     </b-card>
