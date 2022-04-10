863346e0adef2dd9a92428e23306c7bc6cce0bb5
listerily <listerily@outlook.com>
2022-04-10 13:39:36 +0800

    feat: designed statistics page

--- src/pages/admin/statistics-page.vue
+++ src/pages/admin/statistics-page.vue
@@ -1,12 +1,107 @@
 <template>
   <div class="container">
+    <item-select-card api="problem" @select="onItemSelected" :newable="false"></item-select-card>
+    <div v-if="hasItemSelected" class="mt-3">
+      <b-alert show variant="secondary" class="text-center" dismissible fade>提示：统计数据只提供最近的最高分。</b-alert>
+      <b-table hover :items="items" :fields="fields" striped class="text-center mt-4" :busy="isLoading" responsive show-empty>
+        <template #table-busy>
+          <div class="text-center my-2">
+            <b-spinner class="align-middle"></b-spinner>
+            <strong class="ml-4">加载中...</strong>
+          </div>
+        </template>
+        <template #empty>
+          <strong class="text-muted"> 没有相关记录 </strong>
+        </template>
 
+        <template #cell(status_id)="data">
+          <h6><b-link class="text-decoration-none" :href="`/submission/${data.value}`">#{{data.value}}</b-link></h6>
+        </template>
+        <template #cell(status)="data">
+          <h6 :class="`text-dark`">{{data.value}}</h6>
+        </template>
+        <template #cell(uid)="data">
+          <h6>{{`#${uidToStr(data.value)}`}}</h6>
+        </template>
+      </b-table>
+      <div class="container-fluid d-flex justify-content-center mt-4 mb-4">
+        <b-button-group class="center">
+          <b-button variant="info" :disabled="isLoading">
+            <b-spinner small type="grow" v-if="isLoading"></b-spinner>
+            下载数据
+          </b-button>
+          <b-button variant="secondary" :disabled="isLoading">
+            <b-spinner small type="grow" v-if="isLoading"></b-spinner>
+            下载源码
+          </b-button>
+        </b-button-group>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import itemSelectCard from '../../components/admin/admin-problem-select-card'
+import code2str from "@/util/code";
+import status2text from "@/util/status-text";
+import status2variant from "@/util/status-variant";
+import uid2Str from "@/util/uidToStr";
+
 export default {
-  name: "statistics-page"
+  name: "statistics-page",
+  components: {
+    itemSelectCard
+  },
+  data: function () {
+    return {
+      statistics: [],
+      items: [],
+      selectedId: 0,
+      hasItemSelected: false,
+      fields: [
+        { key: 'uid', label: 'UID' },
+        { key: 'nickname', label: '昵称' },
+        { key: 'name', label: '实名' },
+        { key: 'username', label: '用户名' },
+        { key: 'lang', label: '语言' },
+        { key: 'status_id', label: '记录ID' },
+        { key: 'status', label: '状态' },
+        { key: 'score', label: '分数' },
+      ],
+      isLoading: true,
+    }
+  },
+  methods: {
+    onItemSelected: function (id) {
+      this.selectedId = id
+      this.loadSelectedItem()
+    },
+    loadSelectedItem: function () {
+      this.hasItemSelected = true
+      this.isLoading = true
+      this.$http.get(`${window.backendOrigin}/api/admin/problem/id/${this.selectedId}/statistics`).then(res => {
+        this.statistics = res.data
+        this.items = []
+        for(const item of res.data) {
+          this.items.push({ status_id: item.sid, score: item.score, nickname: item.nickname, username: item.email,
+            status: item.status, uid: item.uid, name: item.realname, lang: item.lang })
+        }
+        this.isLoading = false
+      }, e => {
+        console.log(e)
+        this.$bvModal.msgBoxOk(code2str(e.status), {centered: true, title: '载入信息失败'})
+      })
+    },
+    getStatusText: function (status) {
+      return status2text(status)
+    },
+    getStatusVariant: function (status) {
+      return status2variant(status)
+    },
+    uidToStr: function (uid) {
+      return uid2Str(uid)
+    },
+  }
 }
 </script>
 
