1cf39f349bb6edfed9552ad7947fdf1e9904aa53
listerily <listerily@outlook.com>
2022-04-15 14:25:50 +0800

    fix: unable to log in on test servers

--- src/global.js
+++ src/global.js
@@ -1,7 +1,9 @@
 window.backendOrigin = window.location.origin
 window.backendHost = window.location.host
 window.backendPort = ''
+window.frontendCookiesWithSecure = true
 if (process.env.NODE_ENV === 'development') {
   window.backendOrigin = 'http://localhost'
   window.backendHost = 'localhost'
+  window.frontendCookiesWithSecure = false
 }
