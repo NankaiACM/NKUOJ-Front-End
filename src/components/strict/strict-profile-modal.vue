d05329ea195acdc25a9ab6ab3322dff3bfe971cd
listerily <listerily@outlook.com>
2022-05-11 22:40:55 +0800

    feat: added realname row for exam profile

--- src/components/strict/strict-profile-modal.vue
+++ src/components/strict/strict-profile-modal.vue
@@ -1,22 +1,26 @@
 <template>
-<b-modal id="profile-modal" ok-title="关闭" title="考生信息" ok-only centered>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>UID:</b></b-col>
-    <b-col>{{userData.uid}}</b-col>
-  </b-row>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>昵称：</b></b-col>
-    <b-col>{{userData.nickname}}</b-col>
-  </b-row>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>学号：</b></b-col>
-    <b-col>{{studentID}}</b-col>
-  </b-row>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>用户名：</b></b-col>
-    <b-col>{{userData.username}}</b-col>
-  </b-row>
-</b-modal>
+  <b-modal id="profile-modal" ok-title="关闭" title="考生信息" ok-only centered>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>UID:</b></b-col>
+      <b-col>{{ userData.uid }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>昵称：</b></b-col>
+      <b-col>{{ userData.nickname }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>学号：</b></b-col>
+      <b-col>{{ studentID }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>实名：</b></b-col>
+      <b-col>{{ userData.realname }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>用户名：</b></b-col>
+      <b-col>{{ userData.username }}</b-col>
+    </b-row>
+  </b-modal>
 </template>
 
 <script>
@@ -30,6 +34,11 @@ export default {
   methods: {
     show: function () {
       this.$bvModal.show('profile-modal')
+    },
+    loadProfileData: function () {
+      this.$http.get(`${window.backendOrigin}/api/user/id/${this.userData.uid}`).then(res => {
+        this.realname = res.data.realname
+      })
     }
   },
   computed: {
@@ -45,6 +54,7 @@ export default {
   },
   mounted() {
     this.userData = this.$store.getters.getUserData
+    this.loadProfileData()
   }
 }
 </script>
--- src/components/strict/strict-profile-panel.vue
+++ src/components/strict/strict-profile-panel.vue
@@ -1,22 +1,26 @@
 <template>
-<b-card>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>UID:</b></b-col>
-    <b-col>{{userData.uid}}</b-col>
-  </b-row>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>昵称：</b></b-col>
-    <b-col>{{userData.nickname}}</b-col>
-  </b-row>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>学号：</b></b-col>
-    <b-col>{{studentID}}</b-col>
-  </b-row>
-  <b-row class="mb-2">
-    <b-col sm="3" class="text-sm-left"><b>用户名：</b></b-col>
-    <b-col>{{userData.username}}</b-col>
-  </b-row>
-</b-card>
+  <b-card>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>UID:</b></b-col>
+      <b-col>{{ userData.uid }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>昵称：</b></b-col>
+      <b-col>{{ userData.nickname }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>学号：</b></b-col>
+      <b-col>{{ studentID }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>实名：</b></b-col>
+      <b-col>{{ userData.realname }}</b-col>
+    </b-row>
+    <b-row class="mb-2">
+      <b-col sm="3" class="text-sm-left"><b>用户名：</b></b-col>
+      <b-col>{{ userData.username }}</b-col>
+    </b-row>
+  </b-card>
 </template>
 
 <script>
@@ -24,7 +28,15 @@ export default {
   name: "strict-profile-panel",
   data: function () {
     return {
-      userData: {}
+      userData: {},
+      realname: ''
+    }
+  },
+  methods: {
+    loadProfileData: function () {
+      this.$http.get(`${window.backendOrigin}/api/user/id/${this.userData.uid}`).then(res => {
+        this.realname = res.data.realname
+      })
     }
   },
   computed: {
@@ -40,6 +52,7 @@ export default {
   },
   mounted() {
     this.userData = this.$store.getters.getUserData
+    this.loadProfileData()
   }
 }
 </script>
