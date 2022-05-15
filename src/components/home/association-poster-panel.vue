a283fee7ccddb626fd3a4a63af70b916261d0777
listerily <listerily@outlook.com>
2022-05-15 22:06:18 +0800

    updated text

--- src/components/home/association-poster-panel.vue
+++ src/components/home/association-poster-panel.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-card title="NKPC预选赛" sub-title="5月9号正式开赛。扫码了解详情">
+    <b-card title="南开大学 ACM 算法协会" sub-title="欢迎喵">
       <b-link v-b-toggle.collapse-1 class="text-decoration-none text-muted">显示二维码</b-link>
       <b-collapse id="collapse-1" class="mt-2">
         <img class="img-fluid" src="../../assets/qq-group.jpg" alt="QQ Group ID: 1001918732">
