0066f8b19c7f17f592e36ca73beb205910ae60ea
listerily <listerily@outlook.com>
2023-03-13 12:57:15 +0800

    feat: submitting spinner

--- src/components/modal/ModalSubmitProblem.vue
+++ src/components/modal/ModalSubmitProblem.vue
@@ -1,41 +1,61 @@
 <template>
-  <ModalBase v-model="internalShow" :title="`提交题目 #${pid}`"
-             :ok-button="true" ok-class="btn-outline-purple" ok-text="提交"
-             :cancel-button="true" cancel-class="btn-outline-danger" cancel-text="取消"
-             @ok="submit()" @close="dismiss()" @cancel="dismiss()" large>
-    <div class="container">
-      <div class="form-row align-items-center">
-        <div class="container">
-          <form>
-            <div class="mb-3">
-              <label class="form-label">选择语言</label>
-              <select class="form-select" v-model="selectedLanguage">
-                <option selected value="-1">请选择...</option>
-                <option value="0">C++</option>
-                <option value="1">Python</option>
-                <option value="2">Javascript</option>
-                <option value="3">Go</option>
-                <option value="4">Text</option>
-                <option value="5">Pypy3</option>
-              </select>
+  <VueFinalModal class="d-flex justify-content-center align-items-center h-100 w-100"
+                 overlayTransition="vfm-fade" contentTransition="vfm-fade" v-model="internalShow">
+    <div class="modal modal-lg">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{ `提交题目 #${pid}` }}</h5>
+            <button type="button" class="btn-close" @click="dismiss"></button>
+          </div>
+          <div class="modal-body">
+            <div class="container">
+              <div class="form-row align-items-center">
+                <div class="container">
+                  <form>
+                    <div class="mb-3">
+                      <label class="form-label">选择语言</label>
+                      <select class="form-select" v-model="selectedLanguage">
+                        <option selected value="-1">请选择...</option>
+                        <option value="0">C++</option>
+                        <option value="1">Python</option>
+                        <option value="2">Javascript</option>
+                        <option value="3">Go</option>
+                        <option value="4">Text</option>
+                        <option value="5">Pypy3</option>
+                      </select>
+                    </div>
+                    <div class="mb-3">
+                      <label class="form-label">代码</label>
+                      <textarea class="form-control" rows="12" v-model="sourceCode" placeholder="粘贴您的代码"></textarea>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" v-model="codePublic" type="checkbox">
+                      <label class="form-check-label" for="flexCheckChecked">
+                        公开所提交的代码
+                      </label>
+                    </div>
+                    <small class="text-muted">作业、比赛、考试中的题目只有在其截止提交后才可以被他人查看。</small>
+                  </form>
+                </div>
+              </div>
             </div>
-            <div class="mb-3">
-              <label class="form-label">代码</label>
-              <textarea class="form-control" rows="12" v-model="sourceCode" placeholder="粘贴您的代码"></textarea>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" v-model="codePublic" type="checkbox">
-              <label class="form-check-label" for="flexCheckChecked">
-                公开所提交的代码
-              </label>
-            </div>
-            <small class="text-muted">作业、比赛、考试中的题目只有在其截止提交后才可以被他人查看。</small>
-          </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-danger" @click="dismiss" :disabled="submitting">
+              <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" v-if="submitting"></span>
+              取消
+            </button>
+            <button type="button" class="btn btn-outline-purple" @click="submit" :disabled="submitting">
+              <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" v-if="submitting"></span>
+              提交
+            </button>
+          </div>
         </div>
       </div>
     </div>
     <ModalMsgBox ref="submit_problem_msg_box"/>
-  </ModalBase>
+  </VueFinalModal>
   <ModalSubmissionStatusTracing ref="modal_submission_tracing"/>
 </template>
 
@@ -45,10 +65,11 @@ import axios from "axios";
 import ModalMsgBox from "@/components/modal/ModalMsgBox.vue";
 import httpCodeToStr from "@/util/http-code-to-str";
 import ModalSubmissionStatusTracing from "@/components/modal/ModalSubmissionStatusTracing.vue";
+import {VueFinalModal} from "vue-final-modal";
 
 export default {
   name: "ModalSubmitProblem",
-  components: {ModalSubmissionStatusTracing, ModalMsgBox, ModalBase},
+  components: {VueFinalModal, ModalSubmissionStatusTracing, ModalMsgBox, ModalBase},
   props: {
     pid: Number
   },
@@ -60,11 +81,12 @@ export default {
       validateResults: ['', '未填写代码', '未选择语言'],
       selectedLanguage: -1,
       codePublic: "false",
-      languageList: ['c++', 'python', 'javascript', 'go', 'text', 'pypy3']
+      languageList: ['c++', 'python', 'javascript', 'go', 'text', 'pypy3'],
+      submitting: false
     }
   },
   methods: {
-    submit: function (view) {
+    submit: function () {
       const validateCode = this.validateSubmitForm();
       if (validateCode !== 0) {
         this.$refs.submit_problem_msg_box.show('提示', this.validateResults[validateCode]);
@@ -75,15 +97,19 @@ export default {
         lang: this.languageList[this.selectedLanguage],
         code: this.sourceCode,
         share: this.codePublic !== 'false'
-      }
+      };
+      this.submitting = true;
       axios.post(`/api/judge`, requestPackage).then(res => {
         if (res.status !== 200) {
           this.$refs.submit_problem_msg_box.show('提交失败', httpCodeToStr(res.status));
+          this.submitting = false;
         } else {
+          this.submitting = false;
           this.dismiss();
           this.$refs.modal_submission_tracing.show(res.data);
         }
       }, e => {
+        this.submitting = false;
         this.$refs.submit_problem_msg_box.show('提交失败', httpCodeToStr(e.response.status));
       });
     },
@@ -106,5 +132,14 @@ export default {
 </script>
 
 <style scoped>
-
+.modal {
+  position: fixed;
+  display: block;
+}
+.modal-header {
+  border-bottom: none;
+}
+.modal-footer {
+  border-top: none;
+}
 </style>
